f7af6d8ebfeb7ad08eb6e4afdb373eba4cc3ca19
jhoun <jayhoun@gmail.com>
2017-08-23 16:21:45 -0700

    added countdown timer functionality and circle css

---
+++ app.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  function startTimer(duration){
+    var timer = duration;
+    var minutes;
+    var seconds;
+    setInterval(function(){
+      minutes = parseInt(timer / 60, 10);
+      seconds = parseInt(timer % 60, 10);
+
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+
+      $("#countdown").text( minutes + ":" + seconds);
+
+      if (--timer < 0){
+        timer = 0
+      }
+    }, 1000)
+  }
+
+  startTimer(3)
+});
