c5cef9cec15bc2cf0e489eec2ce1ea8ed05200a4
jhoun <jayhoun@gmail.com>
2017-08-24 00:56:54 -0700

    added stop/go to timer

--- app.js
+++ app.js
@@ -1,34 +1,65 @@
 $(document).ready(function() {
+  var clicked = null;
+  var isRunning = true;
+  //when button is pressed
   $(".btn").click(function(e){
-    var initialNumber = parseInt($("#number-break").text());
+    var initialNumberBreak = parseFloat($("#number-break").text());
+    var initialNumberSession = parseFloat($("#number-session").text());
     var id = e.currentTarget.id;
-    if (id === "subtract-break" && initialNumber >= 2){
-      initialNumber--;
+
+    if (id === "subtract-break" && initialNumberBreak >= 2){
+      initialNumberBreak--;
     }
     if (id === "add-break"){
-      initialNumber++;
+      initialNumberBreak++;
     }
-    $("#number-break").text(initialNumber);
+    $("#number-break").text(initialNumberBreak);
+
+    if (id === "subtract-session" && initialNumberSession >= 2){
+      initialNumberSession--;
+    }
+    if (id === "add-session"){
+      initialNumberSession++;
+    }
+
+    $("#number-session").text(initialNumberSession);
+    $("#countdown").text(initialNumberSession + ":00");
   })
-  function startTimer(duration){
+
+    function startTimer(duration){
     var timer = duration;
     var minutes;
     var seconds;
+
     setInterval(function(){
-      minutes = parseInt(timer / 60, 10);
-      seconds = parseInt(timer % 60, 10);
+      if (isRunning) {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
 
-      minutes = minutes < 10 ? "0" + minutes : minutes;
-      seconds = seconds < 10 ? "0" + seconds : seconds;
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
 
-      $("#countdown").text( minutes + ":" + seconds);
+        $("#countdown").text( minutes + ":" + seconds);
 
-      if (--timer < 0){
-        timer = 0
+        if (--timer < 0){
+          timer = 0
+        }
       }
     }, 1000)
   }
 
+  $("#circle").click(function(e){
 
-  startTimer(3)
+    var currentNumber = parseFloat($("#countdown").text().slice(0, -3)) * 60;
+    if (clicked === null){
+      startTimer(currentNumber);
+      clicked = true
+    } else if (clicked) {
+      isRunning = false;
+      clicked = false;
+    } else {
+      isRunning = true;
+      clicked = true;
+    }
+  })
 });
